41e177e64792ff5d727642fb52e26a97fef9cc94
bezilule <88293035+bezilule@users.noreply.github.com>
2023-09-24 21:36:07 -0400

    #18 migrate local state to terraform cloud using terraform cloud block (#19)

--- main.tf
+++ main.tf
@@ -1,4 +1,10 @@
 terraform {
+  cloud {
+    organization = "Enat-Terraform-Bootcamp"
+    workspaces {
+      name = "terra-house-1"
+    }
+  }
   required_providers {
     random = {
       source = "hashicorp/random"
