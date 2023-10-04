13680a07a88b19114b1d0de0581169d262f0dfc0
bezilule <88293035+bezilule@users.noreply.github.com>
2023-10-03 20:04:42 -0400

    #36 only upload index.html if a content version has change. (#37)

--- variabiles.tf
+++ variabiles.tf
@@ -12,3 +12,8 @@ variable "index_html_filepath" {
 variable "error_html_filepath" {
   type        = string
 }
+
+variable "content_version" {
+  description = "The content version must be a positive integer starting at 1."
+  type        = number
+}
