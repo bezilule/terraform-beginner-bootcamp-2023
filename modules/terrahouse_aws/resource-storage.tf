6642d7040e7170482a2be16e35fea18e779dc975
bezilule <bezilule@yahoo.com>
2023-10-06 19:23:40 +0000

    edited resource-storage error file

--- modules/terrahouse_aws/resource-storage.tf
+++ modules/terrahouse_aws/resource-storage.tf
@@ -35,6 +35,19 @@ resource "aws_s3_object" "index_html" {
   }
 }
 
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.bucket
+  key    = "error.html"
+  source = var.error_html_filepath
+  content_type = "text/html"
+
+  etag = filemd5(var.error_html_filepath)
+  lifecycle {
+    replace_triggered_by = [terraform_data.content_version.output]
+    ignore_changes = [etag]
+  }
+}
+
 resource "aws_s3_object" "upload_assets" {
   for_each = fileset("var.assets_path","*.{jpg,png,gif}")
   bucket = aws_s3_bucket.website_bucket.bucket
