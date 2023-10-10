2560231fd94886cfdf5a3f9f6043e13b1b3a869d
bezilule <bezilule@yahoo.com>
2023-10-10 00:55:42 +0000

    #52 test our terra home on terratowns test town

--- main.tf
+++ main.tf
@@ -23,30 +23,31 @@ terraform {
 }
 
 provider "terratowns" {
-  endpoint = "http://localhost:4567/api"
-  user_uuid="e328f4ab-b99f-421c-84c9-4ccea042c7d1" 
-  token="9b49b3fb-b8e9-483c-b703-97ba88eef8e0"
+  endpoint = var.terratowns_endpoint
+  user_uuid = var.teacherseat_user_uuid
+  token = var.terratowns_access_token
 }
 
-#module "terrahouse_aws" {
-#  source = "./modules/terrahouse_aws"
-#  user_uuid = var.user_uuid
-#  bucket_name = var.bucket_name
-#  index_html_filepath = var.index_html_filepath
-#  error_html_filepath = var.error_html_filepath
-#  content_version = var.content_version
-#}
+module "terrahouse_aws" {
+  source = "./modules/terrahouse_aws"
+  user_uuid = var.teacherseat_user_uuid
+  bucket_name = var.bucket_name
+  index_html_filepath = var.index_html_filepath
+ error_html_filepath = var.error_html_filepath
+ content_version = var.content_version
+  assets_path = var.assets_path
+}
 
 resource "terratowns_home" "home" {
-  name = "How to play Arcanum in 2023!"
+  name = "Fall Season!"
   description = <<DESCRIPTION
-Arcanum is a game from 2001 that shipped with alot of bugs.
-Modders have removed all the originals making this game really fun
-to play (despite that old look graphics). This is my guide that will
-show you how to play arcanum without spoiling the plot.
+Fall, or autumn, is a transition from summer to winter marked by shorter days and cooler temperatures. 
+The season displays vibrant leaf colors due to pigment changes. It brings a cooler atmosphere, fallen leaves, 
+and a shift to warmer clothing. Harvests like pumpkins and apples peak, and cultural celebrations like Thanksgiving occur. 
+The fleeting nature of the season also encourages reflection on life's transience, making fall a time to appreciate changing beauty.
 DESCRIPTION
-  #domain_name = module.terrahouse_aws.cloudfront_url
-  domain_name = "3fdq3gz.cloudfront.net"
-  town = "gamers-grotto"
+  domain_name = module.terrahouse_aws.cloudfront_url
+  #domain_name = "3fdq3gz.cloudfront.net"
+  town = "the-nomad-pad"
   content_version = 1
 }
